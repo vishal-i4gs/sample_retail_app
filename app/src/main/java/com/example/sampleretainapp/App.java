eb6936f458708b9e48ea84ec1d430976f3ab730b
Vishal-SlangLabs <slanglabs-vishal@Vishal-SlangLabss-MacBook-Pro.local>
2020-10-15 14:27:11 +0530

    Modifed repository getter method based on the latest requirements for the repository

--- app/src/main/java/com/example/sampleretainapp/App.java
+++ app/src/main/java/com/example/sampleretainapp/App.java
@@ -2,9 +2,21 @@ package com.example.sampleretainapp;
 
 import android.app.Application;
 
+import com.example.sampleretainapp.db.AppDatabase;
+
 public class App extends Application {
 
+    private AppExecutors mAppExecutors;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mAppExecutors = new AppExecutors();
+    }
+
     public Repository getRepository() {
-        return Repository.getInstance(this);
+        return Repository.getInstance(this,
+                AppDatabase.getInstance(this,
+                        mAppExecutors),mAppExecutors);
     }
 }
