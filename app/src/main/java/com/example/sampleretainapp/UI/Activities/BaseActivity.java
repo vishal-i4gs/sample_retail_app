62e9d3510ac725e212153452124770a56c37e423
Vishal-SlangLabs <slanglabs-vishal@Vishal-SlangLabss-MacBook-Pro.local>
2020-10-15 14:33:40 +0530

    Removed deprecated lines and files

--- app/src/main/java/com/example/sampleretainapp/UI/Activities/BaseActivity.java
+++ app/src/main/java/com/example/sampleretainapp/UI/Activities/BaseActivity.java
@@ -99,13 +99,6 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) { // use android.R.id
             dl.openDrawer(Gravity.LEFT);
