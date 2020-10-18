289fbe55880565aceef434964164d7d1260560a1
Vishal-SlangLabs <slanglabs-vishal@Vishal-SlangLabss-MacBook-Pro.local>
2020-10-18 15:33:09 +0530

    Updated OfferActivity to use the latest OfferAdapter and AppViewModel

--- app/src/main/java/com/example/sampleretainapp/UI/Activities/OffersActivity.java
+++ app/src/main/java/com/example/sampleretainapp/UI/Activities/OffersActivity.java
@@ -1,7 +1,6 @@
 package com.example.sampleretainapp.UI.Activities;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,23 +12,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.sampleretainapp.Model.CartItem;
 import com.example.sampleretainapp.Model.Item;
-import com.example.sampleretainapp.Model.OfferItem;
 import com.example.sampleretainapp.R;
-import com.example.sampleretainapp.UI.Adapters.ListAdapter;
+import com.example.sampleretainapp.UI.Adapters.OfferItemsAdapter;
 import com.example.sampleretainapp.UI.ItemClickListener;
-import com.example.sampleretainapp.UI.ViewModel.MainActivityViewModel;
+import com.example.sampleretainapp.UI.ViewModel.AppViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
 public class OffersActivity extends BaseActivity implements ItemClickListener {
 
-    private MainActivityViewModel mainActivityViewModel;
-    private ListAdapter listAdapter;
+    private OfferItemsAdapter listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +31,9 @@ public class OffersActivity extends BaseActivity implements ItemClickListener {
 
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View contentView = inflater.inflate(R.layout.activity_search_list, null, false);
-        ll.addView(contentView, new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT));
+        ll.addView(contentView, new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.MATCH_PARENT,
+                ConstraintLayout.LayoutParams.MATCH_PARENT));
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("My Offers");
@@ -45,27 +41,12 @@ public class OffersActivity extends BaseActivity implements ItemClickListener {
 
         RecyclerView listItemView = contentView.findViewById(R.id.list_item_view);
 
-        mainActivityViewModel = new ViewModelProvider(this).get(
-                MainActivityViewModel.class);
-        mainActivityViewModel.getOfferItems().observe(this,
-                new Observer<List<OfferItem>>() {
-                    @Override
-                    public void onChanged(List<OfferItem> offerItems) {
-                        List<Item> items = new ArrayList<>();
-                        for (OfferItem offerItem : offerItems) {
-                            items.add(offerItem.item);
-                        }
-                        listAdapter.setList(items);
-                    }
+        AppViewModel appViewModel = new ViewModelProvider(this).get(
+                AppViewModel.class);
+        appViewModel.getOfferItems().observe(this,
+                offerItems -> {
+            listAdapter.setList(offerItems);
                 });
-
-        mainActivityViewModel.getCartLiveData().observe(this, new Observer<List<CartItem>>() {
-            @Override
-            public void onChanged(List<CartItem> cartItems) {
-                listAdapter.notifyDataSetChanged();
-            }
-        });
-
 
         FloatingActionButton fab = contentView.findViewById(R.id.fab);
         fab.setOnClickListener(view -> {
@@ -75,22 +56,20 @@ public class OffersActivity extends BaseActivity implements ItemClickListener {
         TextView cartItemCount = findViewById(R.id.cart_item_count);
         cartItemCount.setVisibility(View.GONE);
         fab.setVisibility(View.GONE);
-        mainActivityViewModel.getCartLiveData().observe(this, new Observer<List<CartItem>>() {
-            @Override
-            public void onChanged(List<CartItem> cartItems) {
-                listAdapter.notifyDataSetChanged();
-                if (cartItems.size() == 0) {
-                    cartItemCount.setVisibility(View.GONE);
-                    fab.setVisibility(View.GONE);
-                } else {
-                    cartItemCount.setVisibility(View.VISIBLE);
-                    fab.setVisibility(View.VISIBLE);
-                }
-                cartItemCount.setText(String.format(Locale.ENGLISH, "%d", cartItems.size()));
-            }
-        });
+        appViewModel.getCartItems().observe(this,
+                cartItems -> {
+                    if (cartItems.size() == 0) {
+                        cartItemCount.setVisibility(View.GONE);
+                        fab.setVisibility(View.GONE);
+                    } else {
+                        cartItemCount.setVisibility(View.VISIBLE);
+                        fab.setVisibility(View.VISIBLE);
+                    }
+                    cartItemCount.setText(String.format(Locale.ENGLISH, "%d", cartItems.size()));
+                });
 
-        listAdapter = new ListAdapter(mainActivityViewModel, this);
+        listAdapter = new OfferItemsAdapter(OfferItemsAdapter.Type.VERTICAL_LIST,
+                appViewModel, this);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         listItemView.setLayoutManager(layoutManager);
         listItemView.setItemAnimator(null);
@@ -101,7 +80,7 @@ public class OffersActivity extends BaseActivity implements ItemClickListener {
     @Override
     public void itemClicked(Item item) {
         Intent intent = new Intent(this, ItemActivity.class);
-        intent.putExtra("itemId",item.id);
+        intent.putExtra("itemId", item.id);
         startActivity(intent);
     }
 }
