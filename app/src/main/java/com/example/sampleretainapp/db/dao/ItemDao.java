13268f8e54a6c3ae8ef9406ae6e13e52fffc4ea8
Vishal-SlangLabs <slanglabs-vishal@Vishal-SlangLabss-MacBook-Pro.local>
2020-10-18 14:55:57 +0530

    Added DAO for item entity

---
+++ app/src/main/java/com/example/sampleretainapp/db/dao/ItemDao.java
@@ -0,0 +1,42 @@
+package com.example.sampleretainapp.db.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Transaction;
+
+import com.example.sampleretainapp.Model.Item;
+import com.example.sampleretainapp.Model.ItemOfferCart;
+
+import java.util.List;
+
+@Dao
+public interface ItemDao {
+
+    @Transaction
+    @Query("SELECT * FROM items")
+    LiveData<List<ItemOfferCart>> getItemsAndOffers();
+
+    @Transaction
+    @Query("SELECT * FROM items")
+    LiveData<List<Item>> getItems();
+
+    @Query("DELETE FROM items")
+    public void removeAllItems();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(List<Item> items);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Item item);
+
+    @Transaction
+    @Query("SELECT * FROM items WHERE name LIKE '%' || :search || '%'")
+    LiveData<List<ItemOfferCart>> getItemsAndOffersBasedOnSearch(String search);
+
+    @Transaction
+    @Query("SELECT * FROM items where id = :id")
+    LiveData<ItemOfferCart> getItemForId(String id);
+}
